7667fd73605c333a0b4db2d04882692ada2d0a3c
ton1czech <tonyasek007@gmail.com>
2022-10-26 04:01:01 +0200

    🎁 NEW: inflace table

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import pandas as pd
 import numpy as np
 
 st.title("Inflace v ČR")
+
+data = pd.read_csv('https://raw.githubusercontent.com/ton1czech/inflace-cr-dataset/master/inflace.csv', index_col='rok')
+
+st.subheader('Tabulka inflace')
+st.write(data)
