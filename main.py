ca2dd6ca0a54e85be720ff2009f836362e3152b4
ton1czech <tonyasek007@gmail.com>
2022-10-27 00:15:46 +0200

    👌 IMPROVE: formatting

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import pandas as pd
 import plotly_express as px
 
 # page settings
-st.set_page_config(page_title="Inflace v ČR", page_icon=":chart_with_upwards_trend:")
+st.set_page_config(page_title='Inflace v ČR', page_icon=':chart_with_upwards_trend:')
 
 # sidebar
-st.sidebar.title("Navigace")
-options = st.sidebar.radio("Vyber vizualizaci:", [
+st.sidebar.title('Navigace')
+options = st.sidebar.radio('Vyber vizualizaci:', [
     'Hlavní stránka', 
     'Tabulka dat', 
     'Vývoj inflace od roku 2000',
@@ -18,15 +18,15 @@ options = st.sidebar.radio("Vyber vizualizaci:", [
 df = pd.read_csv('https://raw.githubusercontent.com/ton1czech/inflace-cr-dataset/master/mesicni-inflace.csv')
 
 def table_filtered():
-    st.subheader("Tabulka inflace")
+    st.subheader('Tabulka inflace')
 
     new_df = pd.DataFrame([['vše', 'vše', '']], columns=df.columns).append(df)
 
     col1, col2 = st.columns(2)
     with col1:
-        year = st.selectbox("Filtrovat rok:", new_df['rok'].unique())
+        year = st.selectbox('Filtrovat rok:', new_df['rok'].unique())
     with col2:
-        month = st.selectbox("Filtrovat měsíc:",  new_df['měsíc'].unique())
+        month = st.selectbox('Filtrovat měsíc:',  new_df['měsíc'].unique())
     
     if year == 'vše' and month == 'vše':
         st.dataframe(df)
@@ -42,20 +42,21 @@ def table_filtered():
             st.dataframe(new_df)
 
 def table():
-    st.subheader("Tabulka inflace")
+    st.subheader('Tabulka inflace')
 
     st.dataframe(df)
 
 def inflation_alltime():
-    st.subheader("Vývoj inflace of roku 2000")
+    st.subheader('Vývoj inflace of roku 2000')
 
     df['datum'] = df['rok'].astype(str) + '.' + df['měsíc']
 
-    fig = px.line(df, x='datum', y='procenta', labels=dict(datum="Rok a měsíc", procenta='%'))
+    fig = px.line(df, x='datum', y='procenta', labels=dict(datum='Rok a měsíc', procenta='%'))
 
     config = dict({'scrollZoom': True})
     fig.update_layout(
         dragmode='pan',
+        plot_bgcolor='rgba(0,0,0,0)',
         xaxis=dict(
             showgrid=False,
         ),
@@ -68,22 +69,22 @@ def inflation_alltime():
     st.plotly_chart(fig, config=config)
 
 def inflation_by_year():
-    st.subheader("Vývoj inflace (roční)")
+    st.subheader('Vývoj inflace (roční)')
 
-    year = st.selectbox("Vyber rok:", df['rok'].unique())
+    year = st.selectbox('Vyber rok:', df['rok'].unique())
 
     new_df = df.loc[df['rok'] == year]
 
     st.line_chart(new_df, x='měsíc', y='procenta')
 
 if options == 'Hlavní stránka':
-    st.title("Inflace v ČR")
+    st.title('Inflace v ČR')
 
     col1, col2 = st.columns(2)
     with col1:
         table()
     with col2:
-        st.markdown("Data jsou z českého statistického úřadu _(www.czso.cz/csu/czso/mira_inflace)_. Zde najdete mnou vytvořené **.csv** datasety: _www.github.com/ton1czech/inflace-cr-dataset_")
+        st.markdown('Data jsou z českého statistického úřadu _(www.czso.cz/csu/czso/mira_inflace)_. Zde najdete mnou vytvořené **.csv** datasety: _www.github.com/ton1czech/inflace-cr-dataset_')
     inflation_alltime()
     inflation_by_year()
 elif options == 'Tabulka dat':
