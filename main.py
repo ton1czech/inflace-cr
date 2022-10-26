81795da44d37314a077a48e0eaecec93028d3f63
ton1czech <tonyasek007@gmail.com>
2022-10-26 06:36:46 +0200

    🎁 NEW: title + logo

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title="Inflace v ČR", page_icon=":chart_with_upwards_trend:")
+
 st.title("Inflace v ČR")
 
 data = pd.read_csv('https://raw.githubusercontent.com/ton1czech/inflace-cr-dataset/master/inflace.csv')
-data['datum'] = data['rok'].astype(str) + '.' + data['měsíc']
 
-st.subheader('Tabulka inflace')
+st.subheader("Tabulka inflace")
 st.dataframe(data)
 
-st.subheader('Vývoj inflace')
+data['datum'] = data['rok'].astype(str) + '.' + data['měsíc']
+
+st.subheader("Vývoj inflace")
 st.line_chart(data, x='datum', y='procenta')
