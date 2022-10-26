4d608a7797b6e254f39420d37a41db6026d8047e
ton1czech <tonyasek007@gmail.com>
2022-10-26 06:33:03 +0200

    🎁 NEW: inflation trend graph

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 st.title("Inflace v ČR")
 
-data = pd.read_csv('https://raw.githubusercontent.com/ton1czech/inflace-cr-dataset/master/inflace.csv', index_col='rok')
+data = pd.read_csv('https://raw.githubusercontent.com/ton1czech/inflace-cr-dataset/master/inflace.csv')
+data['datum'] = data['rok'].astype(str) + '.' + data['měsíc']
 
 st.subheader('Tabulka inflace')
-st.write(data)
+st.dataframe(data)
+
+st.subheader('Vývoj inflace')
+st.line_chart(data, x='datum', y='procenta')
